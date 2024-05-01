364074a584be968f2660c8b4f82541391b090cb9
Kape99 <lucapecchi13@gmail.com>
2024-05-01 19:14:12 +0200

    Configuration to publish on pages

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: "/sidereal-confluence-counter/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
