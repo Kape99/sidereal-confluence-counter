4b6123a2ad299865449ff6a73b0f739b2c56a19b
Kape99 <lucapecchi13@gmail.com>
2024-05-01 18:36:18 +0200

    Value computation

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { computed, ref } from 'vue';
+import ResourceCounter from './components/ResourceCounter.vue';
+
+
+const totalScore = computed(() => {
+  return resources.value.reduce((accumulator, resource) => accumulator += (resource.count * resource.convertionRate) / 12, 0)
+})
+
+let id = 0
+const resources = ref([
+  { id: id++, resourceName: 'Victory Points', convertionRate: 12, count: 0 },
+  { id: id++, resourceName: 'Octagons', convertionRate: 6, count: 0 },
+  { id: id++, resourceName: 'Small Cubes', convertionRate: 3, count: 0 },
+  { id: id++, resourceName: 'Large Cubes', convertionRate: 2, count: 0 },
+  { id: id++, resourceName: 'Spaceships', convertionRate: 2, count: 0 },
+])
+</script>
+
+<template>
+  <h1>Total Score: {{ totalScore }}</h1>
+  <ul>
+
+    <li v-for="resource in resources" :key="resource.id">
+      <ResourceCounter @increment="resource.count++" @decrement="resource.count--" :resource="resource.resourceName"
+        :count="resource.count">
+      </ResourceCounter>
+
+    </li>
+  </ul>
+</template>
---
+++ src/components/ResourceCounter.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+    resource: Text,
+    count: Number
+})
+
+function increment() {
+    count++;
+}
+function decrement() {
+    if (count != 0)
+        count--;
+}
+</script>
+
+
+<template>
+    <div>
+        <button @click="$emit('increment')">+</button>
+        <h1>{{ resource }} : {{ count }}</h1>
+        <button @click="$emit('decrement')">-</button>
+    </div>
+</template>
